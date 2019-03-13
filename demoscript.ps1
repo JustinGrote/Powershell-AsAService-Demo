c95ccfa2caaee7afe6a722d6adcbc8ec2ef8872a
JustinGrote <justingrote+github@gmail.com>
2019-03-12 23:24:11 -0700

    Make functions for demoscript

--- demoscript.ps1
+++ demoscript.ps1
@@ -1,8 +1,15 @@
 #Powershell As A Service Demo
 $lines = [Environment]::NewLine + "===========================================================================================" + [Environment]::NewLine
+function startcmd ([scriptblock]$cmd) {
+    write-host -fore Cyan "RUNNING COMMAND: $cmd"
+    & $cmd
+}
+function promptstep ($string) {
+    write-host -fore Green "$lines $string $lines";read-host "Press enter to continue..."
+}
 
-write-host -fore Green "$lines Step 1: Make sure our environment is good $lines"
-Invoke-Build Test
+promptstep "Step 1: Make sure our environment is good"
+startcmd {Invoke-Build Test}
 
 
 
